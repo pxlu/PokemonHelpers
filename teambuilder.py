291e768f63d7019789743b703aa491bea9705da8
Peter Lu <peterlu94@gmail.com>
2022-04-02 02:58:34 -0400

    Fixed convert_pokemondata_into_pkmn, added roster to Team class

--- teambuilder.py
+++ teambuilder.py
@@ -22,53 +22,71 @@ class Team:
   def __init__(self, roster=None):
     self.roster = roster
 
-  def _import_team():
-    # Need to convert team text file into a list of pokemon object
-    '''
-    
-    '''
-    pass
-
 def convert_pokemondata_into_pkmn(pokemondata):
 
     pokemondata = [x.strip() for x in pokemondata]
     name_item_pattern = re.compile('.+ @ .+')
     ability_pattern, nature_pattern = re.compile('Ability: .+'), re.compile('[a-zA-Z]+ Nature')
+    evs_pattern, ivs_pattern = re.compile('EVs: .+'), re.compile('IVs: .+')
+    level_pattern, shiny_pattern = re.compile('Level: .+'), re.compile('Shiny: .+')
+    moveset_pattern = re.compile('- .+')
+
+    pkmn_name, pkmn_item, pkmn_ability, pkmn_nature, pkmn_level = None, None, None, None, None 
+    pkmn_evs, pkmn_ivs = {'HP': 0, 'Atk': 0, 'Def': 0, 'SpA': 0, 'SpD': 0, 'Spe': 0}, {'HP': 31, 'Atk': 31, 'Def': 31, 'SpA': 31, 'SpD': 31, 'Spe': 31}
+    pkmn_moveset = []
 
     for pattern in pokemondata:
       name_item_match, ability_pattern_match, nature_pattern_match = name_item_pattern.match(pattern), ability_pattern.match(pattern), nature_pattern.match(pattern)
+      evs_pattern_match, ivs_pattern_match = evs_pattern.match(pattern), ivs_pattern.match(pattern)
+      level_pattern_match, shiny_pattern_match = level_pattern.match(pattern), shiny_pattern.match(pattern)
+      moveset_pattern_match = moveset_pattern.match(pattern)
+
       if name_item_match:
         pkmn_name, pkmn_item = name_item_match.group(0).split('@')[0], name_item_match.group(0).split('@')[-1]
       if ability_pattern_match:
-        pkmn_ability = ability_pattern_match.group(0).split(':')[1]
+        pkmn_ability = ability_pattern_match.group(0).split(':')[1].strip()
       if nature_pattern_match:
-        pkmn_nature = nature_pattern_match.group(0).split(' ')[0]
+        pkmn_nature = nature_pattern_match.group(0).split(' ')[0].strip()
+      if evs_pattern_match:
+        pkmn_evs_data = [x.strip() for x in evs_pattern_match.group(0).split(':')[-1].split('/')]
+        for ev_pair in pkmn_evs_data:
+          pkmn_evs[ev_pair.split(' ')[-1]] = int(ev_pair.split(' ')[0])
+      if ivs_pattern_match:
+        pkmn_ivs_data = [x.strip() for x in ivs_pattern_match.group(0).split(':')[-1].strip().split('/')]
+        for iv_pair in pkmn_ivs_data:
+          pkmn_ivs[iv_pair.split(' ')[-1]] = int(iv_pair.split(' ')[0])
+      if level_pattern_match:
+        pkmn_level = level_pattern_match.group(0).split(':')[-1].strip()
+      if moveset_pattern_match:
+        pkmn_moveset.append(moveset_pattern_match.group(0)[2:].strip())
 
-    pkmn_evs = {'HP': 0, 'Atk': 0, 'Def': 0, 'SpA': 0, 'SpD': 0, 'Spe': 0}
-    pkmn_evs_data = [x.strip() for x in pokemondata[2].split(':')[-1].split('/')] #e.g EVs: 252 HP / 40 Def / 216 Spe
-    for ev_pair in pkmn_evs_data:
-      pkmn_evs[ev_pair.split(' ')[-1]] = int(ev_pair.split(' ')[0])
-    
-    pkmn_ivs = {'HP': 31, 'Atk': 31, 'Def': 31, 'SpA': 31, 'SpD': 31, 'Spe': 31}
-    pkmn_ivs_data = [x.strip() for x in pokemondata[4].split(':')[-1].strip().split('/') if pokemondata[4].strip()[0:2] == 'IV'] 
-    for iv_pair in pkmn_ivs_data:
-      pkmn_ivs[iv_pair.split(' ')[-1]] = int(iv_pair.split(' ')[0])
-    # npkmn = Pokemon(
-    #   name=pkmn_name
-    # )
-    # return npkmn
-    return Pokemon()
+    return Pokemon(
+      name = pkmn_name,
+      item = pkmn_item,
+      ability = pkmn_ability,
+      evs = pkmn_evs,
+      ivs = pkmn_ivs,
+      level = pkmn_level,
+      nature = pkmn_nature,
+      moveset = pkmn_moveset
+    )
 
 if __name__ == '__main__':
   dex = open('pokedex.json', encoding="utf8")
+  # data.keys() is a list of pokemon names
   data = json.load(dex)
   
   sample_team = open('sampleteam.txt', encoding="utf8").readlines()
   delimiter = '\n'
   # https://stackoverflow.com/questions/15357830/splitting-a-list-based-on-a-delimiter-word
   list_of_pokemon_data = [list(y) for x, y in itertools.groupby(sample_team, lambda z: z == delimiter) if not x]
+  
+  pkmn_list = []
   for pokemon in list_of_pokemon_data:
     pkmn = convert_pokemondata_into_pkmn(pokemon)
-    #print(pkmn.name)
-  # data.keys() is a list of pokemon names
+    pkmn_list.append(pkmn)
+
+  new_team = Team(roster=pkmn_list)
+  print(new_team.roster[0])
+  
   dex.close()
